a3733881d343d89f7b191da7ad5651a4e651ae29
李敏 <beyondmx2@gmail.com>
2016-08-06 15:28:12 +0800

    111

--- app/src/main/java/com/app/http/ApiService.java
+++ app/src/main/java/com/app/http/ApiService.java
@@ -23,5 +23,7 @@ public interface ApiService {
     @GET("http://apis.baidu.com/apistore/stockservice/usastock")
     Observable<HttpResult<Stockinfo>> searchStock(@Query("stockid") String stockids, @Query("list") int list);
 
-    @GET("https://android.shein.com/index.php") Observable<HttpResult<LoginInfo>> emailLogin(@Query("model") String model, @Query("action") String action, @Query("email") String email, @Query("password") String pwd);
+    @GET("https://android.app.com/index.php")
+    Observable<HttpResult<LoginInfo>> emailLogin(@Query("model") String
+                                                         model, @Query("action") String action, @Query("email") String email, @Query("password") String pwd);
 }
